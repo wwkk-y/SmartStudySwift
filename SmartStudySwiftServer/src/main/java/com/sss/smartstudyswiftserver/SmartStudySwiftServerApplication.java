854436c6ef10183ab6068571fa9e9867629e499b
韦永志 <3262954152@qq.com>
2024-11-15 16:13:06 +0800

    feat: init backend

---
+++ SmartStudySwiftServer/src/main/java/com/sss/smartstudyswiftserver/SmartStudySwiftServerApplication.java
@@ -0,0 +1,13 @@
+package com.sss.smartstudyswiftserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStudySwiftServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartStudySwiftServerApplication.class, args);
+    }
+
+}
---
+++ SmartStudySwiftServer/src/test/java/com/sss/smartstudyswiftserver/SmartStudySwiftServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sss.smartstudyswiftserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStudySwiftServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
